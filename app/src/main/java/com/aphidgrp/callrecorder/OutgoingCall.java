762f75ba754f2ff82be95957a5321923fb132837
Sonny Lloyd <git@sonnylloyd.co.uk>
2015-03-03 14:20:27 +0000

    calllistener

--- app/src/main/java/com/aphidgrp/callrecorder/OutgoingCall.java
+++ app/src/main/java/com/aphidgrp/callrecorder/OutgoingCall.java
@@ -3,10 +3,15 @@ package com.aphidgrp.callrecorder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class OutgoingCall extends BroadcastReceiver {
+    @Override
     public void onReceive(Context context, Intent intent) {
         if(intent == null) return;
+        Toast toast = Toast.makeText(context, "outgoing", Toast.LENGTH_LONG);
+        toast.show();
+        return;
         //Intent i = new Intent(CallRecordingService.ACTION);
         //context.startService(i);
     }
