9490bae520d8bb320fec08fb26bebecb08cd94c3
Sonny Lloyd <git@sonnylloyd.co.uk>
2015-03-13 17:24:03 +0000

    started call db

---
+++ app/src/main/java/com/aphidgrp/callrecorder/database/entity/Call.java
@@ -0,0 +1,20 @@
+package com.aphidgrp.callrecorder.database.entity;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by sonny.lloyd on 13/03/2015.
+ */
+public class Call extends SugarRecord<Call>{
+    String number;
+    String created;
+
+    public Call(){
+
+    }
+
+    public Call(String number,String created){
+        this.number = number;
+        this.created = created;
+    }
+}
